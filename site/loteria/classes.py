3a6f80c15060cac343b483c833ca63000f5f6bc8
guilherme <gui.lls@hotmail.com>
2018-09-01 09:42:35 -0300

    Criada versão 2 do sorteador do dia de sorte

--- site/loteria/classes.py
+++ site/loteria/classes.py
@@ -1,6 +1,12 @@
 import math
 import random
 
+class Sorteador(object):
+    def sorteia_indice(self, min, max):
+        if min != max:
+            return random.randint(min, max-1)
+        else:
+            return min
 
 class DezenaDiaSorte(object):
     def __init__(self, dicionario):
@@ -51,7 +57,6 @@ class EscolhasDiaSorte(object):
             resultado.sort()
 
 
-
     def _sorteia_indice(self, min, max):
         if min != max:
             return random.randint(min, max-1)
@@ -68,10 +73,66 @@ class EscolhasDiaSorte(object):
         return 0
 
 
+class SorteioDiaSorteV2Request(object):
+    def __init__(self):
+        self.listaFixos = []
+        self.listaQtdSorteaveis = []
+        self.qtdJogos = 0
+
+
 class SorteioDiaSorteV2(object):
     def __init__(self):
+        self.sorteaveisLinha = []
+        self.sorteaveisLinha.append(list(range(0, 11)))
+        self.sorteaveisLinha.append(list(range(11, 21)))
+        self.sorteaveisLinha.append(list(range(21, 32)))
         self.resultados = []
+        self.request = SorteioDiaSorteV2Request()
+        self.sorteador = Sorteador()
 
-    def monta_resultado(self, lista_valores : list):
-        pass
+    def monta_resultados(self, request: SorteioDiaSorteV2Request):
+        self.request = request
 
+        for indice in range(0, self.request.qtdJogos):
+            resultado = []
+            igual_antigo = False
+            self._insere_numeros_fixos_resultado(resultado)
+            self._sorteia_jogo(resultado)
+            resultado.sort()
+            for resultado_antigo in self.resultados:
+                if resultado_antigo == resultado:
+                    igual_antigo = True
+            if not igual_antigo:
+                self.resultados.append(resultado)
+
+    def _sorteia_jogo(self, resultado: list):
+        for indice_linha, linha in enumerate(self.sorteaveisLinha):
+            max_sorteio_linha = self.request.listaQtdSorteaveis[indice_linha]
+            sorteaveis = linha.copy()
+            qtd_removidos = self._remove_fixos_sorteaveis(sorteaveis)
+            qtd_sorteados = 0
+            max_sorteio_linha = max_sorteio_linha - qtd_removidos
+
+            while qtd_sorteados < max_sorteio_linha:
+                indice_sorteado = self.sorteador.sorteia_indice(0, len(sorteaveis))
+                resultado.append(sorteaveis[indice_sorteado])
+                sorteaveis.pop(indice_sorteado)
+                qtd_sorteados += 1
+
+    def _insere_numeros_fixos_resultado(self, resultado: list):
+        for fixo in self.request.listaFixos:
+            resultado.append(fixo)
+
+    def _remove_fixos_sorteaveis(self, sorteaveis: list):
+        qtd_removidos = 0
+        for fixo in self.request.listaFixos:
+            if fixo in sorteaveis:
+                indice_fixo = sorteaveis.index(fixo)
+                sorteaveis.pop(indice_fixo)
+                qtd_removidos += 1
+
+        return qtd_removidos
+
+    def _print_resultados(self):
+        for resultado in self.resultados:
+            print(resultado)
